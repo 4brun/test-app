b88163cd8a8b56517de7268c52e647073d5862cc
Dmitriy <dabrun@gmail.com>
2022-07-31 17:28:34 +0300

    decomposition Card comp

--- test-app/src/UI/Card.vue
+++ test-app/src/UI/Card.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="content">
     <div class="content__item" v-for="item in items" :key="item.id">
-      <div v-if="item.name && item.body">
+      <div v-if="item.username">
+        <h4>{{ item.username }}</h4>
+        <p>{{ item.name }}</p>
+      </div>
+      <div v-else-if="item.name && item.body">
         <h4>{{ item.name }}</h4>
         <p><strong>Сообщение :</strong> {{ item.body }}</p>
       </div>
       <div v-else-if="item.title">
         <h4>{{ item.title }}</h4>
         <p><strong>Описание :</strong> {{ item.body }}</p>
-      </div>
-      <div v-else-if="item.username">
-        <h4>{{ item.username }}</h4>
-        <p>{{ item.name }}</p>
       </div>
     </div>
   </div>
